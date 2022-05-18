098b3d1d196ad69710c225143726ff70bda2261b
fortheXP <89803424+fortheXP@users.noreply.github.com>
2022-05-18 20:36:15 +0530

    Update c1-providers.tf

--- c1-providers.tf
+++ c1-providers.tf
@@ -12,8 +12,8 @@ terraform {
 provider "azurerm" {
     features {}
   subscription_id = "c3978335-fc70-4e86-a87b-2ab9c8a8cf50"
-  client_id       = var.clinet_secret_id
-  client_secret   = "138dd384-ab12-4dfc-9b2b-65607a7c9665"
+  client_id       = "138dd384-ab12-4dfc-9b2b-65607a7c9665"
+  client_secret   = var.clinet_secret_id
   tenant_id       = "e2268bb0-10f8-462f-bfe2-1fb1500894a3"
 }
 
